bc26748bc2e8a0f8f8d30c18aff2f997d1d72f02
Onyx <dev@onyx.app>
2026-04-21 19:06:10 +0530

    fix: silence React Router v7 future flag warning

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,7 +3,7 @@ import { AppRouter } from './router';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AppRouter />
     </BrowserRouter>
   );
